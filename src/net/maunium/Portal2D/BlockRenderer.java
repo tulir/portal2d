c44a32b335413dc93e52a957766d9a866b8f0c49
Tulir293 <tulir293@maunium.net>
2015-08-21 22:50:51 +0300

    Add javadoc

--- src/net/maunium/Portal2D/BlockRenderer.java
+++ src/net/maunium/Portal2D/BlockRenderer.java
@@ -15,13 +15,23 @@ import org.newdawn.slick.Image;
  * @since 0.1
  */
 public class BlockRenderer {
+	/** Contains the BlockType -> Image mappings. */
 	private static Map<BlockType, Image> images = new HashMap<BlockType, Image>();
+	/** Contains the Color -> BlockType mappings. */
 	private static Map<Color, BlockType> colors = new HashMap<Color, BlockType>();
 	
+	/**
+	 * Render the the texture of the given block type at the given coordinates using the given Graphics object.
+	 */
 	public static void render(Graphics g, BlockType bt, int x, int y) {
 		g.drawImage(images.get(bt), x * 32, y * 32);
 	}
 	
+	/**
+	 * Get the block type matching the given color.
+	 * 
+	 * @return The block type, or null if color couldn't be identified.
+	 */
 	public static BlockType getBlockType(int r, int g, int b) {
 		Color c = new Color(r, g, b);
 		for (Entry<Color, BlockType> e : colors.entrySet()) {
@@ -30,11 +40,17 @@ public class BlockRenderer {
 		return null;
 	}
 	
+	/**
+	 * Map the given color and image to the given block type.
+	 */
 	public static void loadImage(BlockType bt, Color c, Image i) {
 		images.put(bt, i);
 		colors.put(c, bt);
 	}
 	
+	/**
+	 * Temporary enum to store the type of the block. This should be changed to something more modular.
+	 */
 	public static enum BlockType {
 		LIGHT, DARK, SPIKE;
 	}
