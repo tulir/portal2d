61e57721161e0d93c7e7cbaf2927dd0b2422574d
Tulir293 <tulir293@maunium.net>
2015-08-21 21:54:25 +0300

    Add libraries, natives and base game code.

--- src/net/maunium/Portal2D/Portal2D.java
+++ src/net/maunium/Portal2D/Portal2D.java
@@ -1,5 +1,37 @@
 package net.maunium.Portal2D;
 
-public class Portal2D {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
+public class Portal2D extends BasicGame {
+	public Portal2D() {
+		super("Portal 2(D)");
+	}
+	
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+	
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+	
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+//		g.drawString("Mau", 100, 100);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			AppGameContainer agc;
+			agc = new AppGameContainer(new Portal2D());
+			agc.setDisplayMode(640, 480, false);
+			agc.start();
+		} catch (SlickException e) {
+			System.err.println("Fatal error:");
+			e.printStackTrace();
+		}
+		
+	}
 }
