297a46bf940c4d508369fe4f0111a1941d67d132
Antti <antti.roysko@saunalahti.fi>
2015-08-22 01:03:29 +0300

    Added rayTrace method

--- src/net/maunium/Portal2D/Map.java
+++ src/net/maunium/Portal2D/Map.java
@@ -6,6 +6,7 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 import net.maunium.Portal2D.Renderer.BlockType;
+import net.maunium.Portal2D.Vector.SideHit;
 
 /**
  * Container for maps.
@@ -66,5 +67,92 @@ public class Map {
 	
 	public Player getPlayer() {
 		return p;
+	}
+	
+	private Vector rayTrace(double angle, int playerX, int playerY) {
+		if (angle % 90 == 0) {
+			angle += 1 / 10000;
+		}
+		Vector portalHit = new Vector(0, 0, Vector.SideHit.TOP);
+		double b = Math.tan(Math.toRadians(angle));
+		double c = playerY - b * playerX;
+		
+		Vector directionVector = new Vector(0, 0, Vector.SideHit.TOP);
+		
+		if (angle < 180) {
+			directionVector.y = 1;
+		} else {
+			directionVector.y = -1;
+		}
+		if (angle > 90 && angle < 270) {
+			directionVector.x = -1;
+		} else {
+			directionVector.x = 1;
+		}
+		
+		double currentX = playerX;
+		int nextVerticalLimit = 0; // Y-position of the limit
+		int nextHorizontalLimit = 0; // X-position of the limit
+		double distToVertLim;
+		double distToHorLim;
+		while (true) {
+			// Getting the next limits.
+			// x
+			if (currentX % 32 == 0) {
+				nextHorizontalLimit = (int) (currentX / 32 + directionVector.x) * 32;
+			} else {
+				nextHorizontalLimit = (int) (currentX - currentX % 32);
+				if (directionVector.x > 0) {
+					nextHorizontalLimit += 32;
+				}
+			}
+			
+			// y
+			if ((currentX * b + c) % 32 == 0) {
+				nextVerticalLimit = (int) ((currentX * b + c) / 32 + directionVector.y) * 32;
+			} else {
+				nextVerticalLimit = (int) (currentX * b + c - (currentX * b + c) % 32);
+				if (directionVector.y > 0) {
+					nextVerticalLimit += 32;
+				}
+			}
+			
+			// Calculating the distances to the limits.
+			distToVertLim = Math.abs((nextVerticalLimit - b * currentX - c) / b - currentX);
+			distToHorLim = Math.abs(nextHorizontalLimit - currentX);
+			
+			// Testing which limit is closer.
+			if (distToVertLim <= distToHorLim) {
+				Renderer.BlockType blockType = blocks[(int) ((currentX - currentX % 32) / 32)][nextVerticalLimit / 32 + directionVector.y];
+				playerX += directionVector.y * distToVertLim;
+				if (blockType == Renderer.BlockType.LIGHT) {
+					// 0 top 1 left 2 bottom 3 right
+					portalHit.sideHit = SideHit.fromInt(directionVector.y + 1);
+					// We get the x and y values of the blockwe're in.
+					portalHit.x = (int) ((currentX - currentX % 32) / 32);
+					portalHit.y = nextVerticalLimit / 32 + directionVector.y;
+					return portalHit;
+					// It's either null (there's nothing there) or there's a place we can add a portal or
+					// There is a place we can't add a portal to. So, If we can't add a portal there we
+					// Just return.
+				} else if (blockType != null) return null;
+			} else {
+				// We get the block that is at the position we crashed to.
+				Renderer.BlockType blockType = blocks[nextHorizontalLimit / 32 + directionVector.x][(int) ((b * currentX + c - (b * currentX + c) % 32) / 32)];
+				playerX += directionVector.x * distToVertLim;
+				// If we can place a portal on the surface, we do so, returning the surface.
+				if (blockType == Renderer.BlockType.LIGHT) {
+					// 0 top 1 left 2 bottom 3 right
+					portalHit.sideHit = SideHit.fromInt(directionVector.x + 2);
+					portalHit.x = nextHorizontalLimit / 32 + directionVector.x;
+					// From x we calculate y.
+					portalHit.y = (int) ((b * currentX + c - (b * currentX + c) % 32) / 32);
+					return portalHit;
+					// It's either null (there's nothing there) or there's a place we can add a portal or
+					// There is a place we can't add a portal to. So, If we can't add a portal there we
+					// Just return.
+				} else if (blockType != null) return null;
+			}
+		}
 	}
 }
