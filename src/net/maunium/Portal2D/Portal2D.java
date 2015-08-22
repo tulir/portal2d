2b7eef6999c2b9a0405662ac3d2b426971940006
Tulir293 <tulir293@maunium.net>
2015-08-22 20:47:41 +0300

    Comments

--- src/net/maunium/Portal2D/Portal2D.java
+++ src/net/maunium/Portal2D/Portal2D.java
@@ -27,6 +27,7 @@ import net.maunium.Portal2D.Map.PortalBullet;
  * @since 0.1
  */
 public class Portal2D extends StateBasedGame {
+	/* Default tile IDs */
 	public static final int TILE_DARK = 1, TILE_LIGHT = 2, TILE_POINT = 3, TILE_FINISH = 4, TILE_BOMB = 5, TILE_NONE = 0;
 	
 	public Portal2D() {
@@ -34,21 +35,28 @@ public class Portal2D extends StateBasedGame {
 	}
 	
 	@Override
-	public void initStatesList(GameContainer container) throws SlickException {
+	public void initStatesList(GameContainer gc) throws SlickException {
+		// Add the menu state.
 		addState(new Menu());
 		
+		// Register the default blocks.
 		BlockRegistry.registerBlock(TILE_DARK, new Color(50, 50, 50), getImage("blocks/tile_dark"), true);
 		BlockRegistry.registerBlock(TILE_LIGHT, new Color(200, 200, 200), getImage("blocks/tile_light"), true);
 		BlockRegistry.registerBlock(TILE_POINT, new Color(0, 100, 0), getImage("blocks/point"), false);
 		BlockRegistry.registerBlock(TILE_FINISH, new Color(0, 255, 0), getImage("blocks/finish"), false);
 		BlockRegistry.registerBlock(TILE_BOMB, new Color(255, 0, 255), getImage("blocks/bomb"), true);
 		
+		// Load the images for the portal bullets.
 		PortalBullet.BLUE_BULLET = getImage("bullet_blue");
 		PortalBullet.ORANGE_BULLET = getImage("bullet_orange");
 		
 		int i = 100;
+		
+		// Loop through map files.
 		for (File f : getMaps()) {
+			// Get the name of the map.
 			String name = f.getName().split(Pattern.quote("."), 3)[1];
+			// Try to add the map as a state.
 			try {
 				addState(new Map(this, new Image(new FileInputStream(f), name, false), name, i++));
 			} catch (FileNotFoundException e) {
@@ -58,20 +66,33 @@ public class Portal2D extends StateBasedGame {
 		}
 	}
 	
+	/**
+	 * Get map files.
+	 */
 	public List<File> getMaps() {
+		// Create an ArrayList which will store the files.
 		List<File> files = new ArrayList<File>();
+		// Get the file pointing to the res/maps directory embedded in the jar.
 		File f = new File(ResourceLoader.getResource("res/maps").getFile());
+		// Loop through the files in the res/maps directory.
 		for (File ff : f.listFiles())
+			// Add each file to the file list.
 			files.add(ff);
+			
+		// Get the ~/.portal2d directory.
 		f = new File(System.getProperty("user.home") + "/.portal2d");
-		
-		if (f.isFile()) f.delete();
+		// Make sure it's not something else than a directory.
+		if (!f.isDirectory()) f.delete();
+		// If it doesn't exist, create it.
 		if (!f.exists()) f.mkdir();
+		// Loop through the files and add them to the list.
 		for (File ff : f.listFiles())
 			files.add(ff);
 			
+		// Sort the maps.
 		Collections.sort(files);
 		
+		// Return the list.
 		return files;
 	}
 	
