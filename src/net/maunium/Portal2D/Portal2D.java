7a5b894762fa1aee80ef590b6ec0246014eb9985
Tulir293 <tulir293@maunium.net>
2015-08-22 14:46:01 +0300

    Find maps from the jar and ~/.portal2d and add them as states.

--- src/net/maunium/Portal2D/Portal2D.java
+++ src/net/maunium/Portal2D/Portal2D.java
@@ -1,5 +1,12 @@
 package net.maunium.Portal2D;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -25,10 +32,37 @@ public class Portal2D extends StateBasedGame {
 	
 	@Override
 	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new Menu());
+		
 		BlockRenderer.loadImage(BlockType.DARK, new Color(50, 50, 50), getImage("blocks/tile_dark"));
 		BlockRenderer.loadImage(BlockType.LIGHT, new Color(200, 200, 200), getImage("blocks/tile_light"));
-		addState(new Menu());
-		addState(new Map(this, getImage("maps/map1"), 1));
+		BlockRenderer.loadImage(BlockType.POINT, new Color(0, 100, 0), getImage("blocks/point"));
+		BlockRenderer.loadImage(BlockType.FINISH, new Color(0, 255, 0), getImage("blocks/finish"));
+		BlockRenderer.loadImage(BlockType.SPIKE, new Color(0, 255, 0), getImage("blocks/bomb"));
+		
+		int i = 99;
+		for (File f : getMaps()) {
+			String name = f.getName().split(Pattern.quote("."), 3)[2];
+			try {
+				addState(new Map(this, new Image(new FileInputStream(f), name, false), name, i++));
+			} catch (FileNotFoundException e) {
+				System.err.println("Failed to load map " + name + ":");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public List<File> getMaps() {
+		List<File> files = new ArrayList<File>();
+		File f = new File(ResourceLoader.getResource("res/maps").getFile());
+		for (File ff : f.listFiles())
+			files.add(ff);
+		f = new File(System.getProperty("user.home") + "/.portal2d");
+		if (f.isFile()) f.delete();
+		if (!f.exists()) f.mkdir();
+		for (File ff : f.listFiles())
+			files.add(ff);
+		return files;
 	}
 	
 	/**
